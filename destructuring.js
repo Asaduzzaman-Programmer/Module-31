c00ac887485c09cc0e2198bf715bedff99c0c77f
asad <asaduzzamanprogrammer@gmail.com>
2024-02-23 02:10:23 +0600

    map pelam na

--- destructuring.js
+++ destructuring.js
@@ -1,35 +1,35 @@
 
 
-const data = [{name:'abul', addess:'kabul', home:'mokbul'}]
+const data = [{ name: 'abul', addess: 'kabul', home: 'mokbul' }]
 // console.log(data[0].name)
 
 
-const fata = [{bal:"nai", isTrue: true}]
+const fata = [{ bal: "nai", isTrue: true }]
 // console.log(fata[0].isTrue)
 
 const laptop = {
-    count : 6500,
+    count: 6500,
     brand: [
-        {id: 1, name: 'lenovo', price: 70000},
-        {id: 2, name: 'asus', price: 90000},
-        {id: 3, name: 'mac', price: 190000}
+        { id: 1, name: 'lenovo', price: 70000 },
+        { id: 2, name: 'asus', price: 90000 },
+        { id: 3, name: 'mac', price: 190000 }
     ]
 }
 
 // console.log(laptop.brand[1].price)
 
 const user = {
-    id: 5012, 
+    id: 5012,
     addess: {
         street: {
             first: "sultan mor",
-            second:"Daka mor",
-            third:{
+            second: "Daka mor",
+            third: {
                 start: "khulna",
-                end:"barishal"
+                end: "barishal"
             }
         }
     }
 }
-console.log( user.id)
+console.log(user.id)
 console.log(user.addess.street.sawon?.third.end);
---
+++ maf.js
@@ -0,0 +1,12 @@
+const sonkha = [2, 4, 6, 8];
+
+const sonkhaDouble = [];
+
+for (const num of sonkha){
+    // console.log(num);
+    const mun = num*2
+    // console.log(mun);
+    sonkhaDouble.push(mun)
+}
+
+// console.log(...sonkhaDouble)
