7cd2e65cdc5b0a399121a709e2de7541c23aac32
asad <asaduzzamanprogrammer@gmail.com>
2024-02-23 01:41:06 +0600

    double

--- destructuring.js
+++ destructuring.js
@@ -1,8 +1,35 @@
 
 
 const data = [{name:'abul', addess:'kabul', home:'mokbul'}]
-console.log(data[0].name)
+// console.log(data[0].name)
 
 
 const fata = [{bal:"nai", isTrue: true}]
-console.log(fata[0].isTrue)
+// console.log(fata[0].isTrue)
+
+const laptop = {
+    count : 6500,
+    brand: [
+        {id: 1, name: 'lenovo', price: 70000},
+        {id: 2, name: 'asus', price: 90000},
+        {id: 3, name: 'mac', price: 190000}
+    ]
+}
+
+// console.log(laptop.brand[1].price)
+
+const user = {
+    id: 5012, 
+    addess: {
+        street: {
+            first: "sultan mor",
+            second:"Daka mor",
+            third:[{
+                start: "khulna",
+                end:"barishal"
+            }]
+        }
+    }
+}
+
+console.log(user.addess.street.third[0].end);
