80e136eddac51def97248d2d2c78284004222ec5
asad <asaduzzamanprogrammer@gmail.com>
2024-02-24 21:40:16 +0600

    1-st

---
+++ all.js
@@ -0,0 +1,13 @@
+const sonkha = [2, 4, 6, 8];
+
+const sonkhaDouble = [];
+
+for (const num of sonkha){
+    // console.log(num);
+    const mun = num*2
+    // console.log(mun);
+    sonkhaDouble.push(mun)
+}
+
+// console.log(...sonkhaDouble)
+
