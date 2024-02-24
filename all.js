aad9f959200cc18723ccd1520c670eae4391c36a
asad <asaduzzamanprogrammer@gmail.com>
2024-02-24 21:41:04 +0600

    2-st

--- all.js
+++ all.js
@@ -11,3 +11,37 @@ for (const num of sonkha){
 
 // console.log(...sonkhaDouble)
 
+
+const numbers = [2, 4, 6, 8];
+
+// function doubleIt(num){
+//     console.log('num now', num)
+//     return num*2;
+// }
+
+// const result = numbers.map(doubleIt)
+// console.log(result);
+
+// function doubleIt(num){
+//     console.log('num of num', num);
+//     return num*2;
+// }
+
+// const result = numbers.map(doubleIt)
+// console.log(result)
+
+// function doubleIt(num){
+//     console.log('num of num', num);
+//     return num*2;
+// }
+
+// const result = numbers.map(doubleIt)
+// console.log(result);
+
+function doubleIt(num){
+    console.log("num of num", num);
+    return num*2;
+}
+
+const result = numbers.map(doubleIt)
+console.log(result);
