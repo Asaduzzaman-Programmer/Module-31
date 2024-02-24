d53af9d84237988a0416a710969caea1bb5aaf48
asad <asaduzzamanprogrammer@gmail.com>
2024-02-24 21:41:46 +0600

    3-st

--- all.js
+++ all.js
@@ -45,3 +45,11 @@ function doubleIt(num){
 
 const result = numbers.map(doubleIt)
 console.log(result);
+const numbers = [2, 4, 6, 8, 10];
+
+const double = numbers.map(num => num * num);
+console.log(double)
+const bonus = double.map(x => x + 5)
+console.log(bonus);
+const bonus2 = numbers.map(num => num+5)
+console.log(bonus)
